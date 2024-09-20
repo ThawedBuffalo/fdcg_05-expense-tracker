f8d0d73875bfd94545ba2d6b7728bb5d29f92b29
JC <thawedbuffalo@gmail.com>
2024-09-20 06:22:36 -0400

    section 5: chapter 30

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -11,28 +11,26 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge),
             const SizedBox(height: 4),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
                 const Spacer(),
-                  Row(
-                    children: [
-                      Icon(categoryIcons[expense.category]),
-                      const SizedBox(width: 8,),
-                      Text(expense.formattedDate),
-                    ],
-                  ),
-
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(expense.formattedDate),
+                  ],
+                ),
               ],
             ),
           ],
