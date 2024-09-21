fd9fe96ad806c13e95e48049298c745c81b8fc39
JC <thawedbuffalo@gmail.com>
2024-09-21 07:01:33 -0400

    section 5: chapter 32

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -6,12 +6,7 @@ final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category {
-  food,
-  travel,
-  leisure,
-  work
-}
+enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
@@ -21,13 +16,12 @@ const categoryIcons = {
 };
 
 class Expense {
-
-  Expense({
-    required this.title,
-    required this.amount,
-    required this.date,
-    required this.category
-  }) : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
@@ -37,5 +31,22 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
